623f6b9434147106473b25c157aa8a3f84626678
Jessica Pamanian <jessicaca@Jessicas-MBP.fios-router.home>
2019-10-17 12:29:06 -0400

    updates maximum-subarray

--- leetcode/maximum-subarray.js
+++ leetcode/maximum-subarray.js
@@ -17,7 +17,7 @@ const maxSubarray = (array, k) => {
   for (var i = 0; i < limit; i++) {
     const nums = Array(k)
       .fill(null)
-      .map((_, idx) => array[i+idx]);
+      .map((_, idx) => array[i + idx]);
 
     res.push(nums.reduce((acc, curr) => {
       acc += curr;
@@ -25,7 +25,43 @@ const maxSubarray = (array, k) => {
     }, 0));
   }
 
-   return res.sort((a, b) => a-b).pop() / k;
+  return res.sort((a, b) => a - b).pop() / k;
 };
 
-console.log(maxSubarray([1,12,-5,-6,50,3,0], 4));
+console.log(maxSubarray([1, 12, -5, -6, 50, 3, 0], 4));
+
+/*
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+Example:
+
+Input: [-2,1,-3,4,-1,2,1,-5,4],
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+
+*/
+var maxSubArray2 = function (nums) {
+  if (!nums.length) {
+    return 0;
+  }
+
+  if (nums.length === 1) {
+    return nums[0];
+  }
+
+  let max = nums[0];
+
+  for (var i = 0; i < nums.length; i++) {
+    let j, sum = nums[i];
+    if (sum > max) {
+      max = sum;
+    }
+    for (j = i + 1; j < nums.length; j++) {
+      sum += nums[j];
+      if (sum > max) {
+        max = sum;
+      }
+    }
+  }
+  return max;
+}
